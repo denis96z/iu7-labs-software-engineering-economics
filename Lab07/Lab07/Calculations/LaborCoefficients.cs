ec72ffd3bc806c5a02b0d549461953e4ad8fa020
denis96z <zinoviev.denis@yandex.ru>
2018-04-28 02:27:33 +0300

    Config loader implemented.

--- Lab07/Lab07/Calculations/LaborCoefficients.cs
+++ Lab07/Lab07/Calculations/LaborCoefficients.cs
@@ -4,62 +4,60 @@ namespace Lab07.Calculations
 {
     public enum LaborCoeffLevel
     {
-        SuperLow, Low, Normal, High, SuperHigh
-    }
-
-    public static class LaborCoeffLevelDecoder
-    {
-        public static double GetPercent(LaborCoeffLevel level)
-        {
-            switch (level)
-            {
-                case LaborCoeffLevel.SuperLow:
-                    return 0.00;
-
-                case LaborCoeffLevel.Low:
-                    return 0.25;
-
-                case LaborCoeffLevel.Normal:
-                    return 0.50;
-
-                case LaborCoeffLevel.High:
-                    return 0.75;
-
-                case LaborCoeffLevel.SuperHigh:
-                    return 1.00;
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(level), level, null);
-            }
-        }
+        VeryLow, Low, Normal, High, VeryHigh, SuperHigh
     }
 
     public struct LaborCoeff
     {
         public string Name { get; }
-        public double MinValue { get; }
-        public double MaxValue { get; }
-        public bool Reversed { get; }
 
-        public LaborCoeff(string name, double min, double max)
+        public double? VeryLowValue { get; set; }
+        public double? LowValue { get; set; }
+        public double? NormalValue { get; set; }
+        public double? HighValue { get; set; }
+        public double? VeryHighValue { get; set; }
+        public double? SuperHighValue { get; set; }
+
+        public bool Reversed { get; set; }
+
+        public LaborCoeff(string name)
         {
             Name = name;
-            MinValue = min;
-            MaxValue = max;
             Level = LaborCoeffLevel.Normal;
-            Reversed = min > max;
+            VeryLowValue = LowValue = NormalValue =
+                HighValue = VeryHighValue = SuperHighValue = 1;
+            Reversed = false;
         }
 
         public LaborCoeffLevel Level { get; set; }
 
-        public double Value
+        public double? Value
         {
             get
             {
-                var p = LaborCoeffLevelDecoder.GetPercent(Level);
-                return Reversed
-                    ? MaxValue + (MinValue - MaxValue) * p
-                    : MinValue + (MaxValue - MinValue) * p;
+                switch (Level)
+                {
+                    case LaborCoeffLevel.VeryLow:
+                        return VeryLowValue;
+
+                    case LaborCoeffLevel.Low:
+                        return LowValue;
+
+                    case LaborCoeffLevel.Normal:
+                        return NormalValue;
+
+                    case LaborCoeffLevel.High:
+                        return HighValue;
+
+                    case LaborCoeffLevel.VeryHigh:
+                        return VeryHighValue;
+
+                    case LaborCoeffLevel.SuperHigh:
+                        return SuperHighValue;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
             }
         }
     }
---
+++ Lab07/Lab07/IO/Config/LaborCoeffLoader.cs
@@ -0,0 +1,77 @@
+﻿using Lab07.Calculations;
+
+namespace Lab07.IO.Config
+{
+    public class LaborCoeffLoader
+    {
+        public LaborCoeff[] GetDefault()
+        {
+            return new[]
+            {
+                new LaborCoeff("PERS")
+                {
+                    VeryLowValue = 1.62,
+                    LowValue = 1.26,
+                    NormalValue = 1.00,
+                    HighValue = 0.83,
+                    VeryHighValue = 0.63,
+                    SuperHighValue = 0.5
+                },
+                new LaborCoeff("RCPX")
+                {
+                    VeryLowValue = 0.60,
+                    LowValue = 0.83,
+                    NormalValue = 1.00,
+                    HighValue = 1.33,
+                    VeryHighValue = 1.91,
+                    SuperHighValue = 2.72
+                },
+                new LaborCoeff("RUSE")
+                {
+                    VeryLowValue = null,
+                    LowValue = 0.95,
+                    NormalValue = 1.00,
+                    HighValue = 1.07,
+                    VeryHighValue = 1.15,
+                    SuperHighValue = 1.24
+                },
+                new LaborCoeff("PDIF")
+                {
+                    VeryLowValue = null,
+                    LowValue = 0.87,
+                    NormalValue = 1.00,
+                    HighValue = 1.29,
+                    VeryHighValue = 1.81,
+                    SuperHighValue = 2.61
+                },
+                new LaborCoeff("PREX")
+                {
+                    VeryLowValue = 1.33,
+                    LowValue = 1.22,
+                    NormalValue = 1.00,
+                    HighValue = 0.87,
+                    VeryHighValue = 0.74,
+                    SuperHighValue = 0.62
+                },
+                new LaborCoeff("FCIL")
+                {
+                    VeryLowValue = 1.30,
+                    LowValue = 1.10,
+                    NormalValue = 1.00,
+                    HighValue = 0.87,
+                    VeryHighValue = 0.73,
+                    SuperHighValue = 0.62
+                },
+                new LaborCoeff("SCED")
+                {
+                    VeryLowValue = 1.43,
+                    LowValue = 1.14,
+                    NormalValue = 1.00,
+                    HighValue = 1.00,
+                    VeryHighValue = 1.00,
+                    SuperHighValue = null
+                }
+            };
+        }
+    }
+}
